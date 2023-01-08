fb3eb861f44832d2e7119a0de1db61f374388fba
Mark Lee <mlee.aus@gmail.com>
2023-01-09 00:18:44 +1100

    feat: add ability to disable file logging. on by default

--- tiny-logger.ts
+++ tiny-logger.ts
@@ -1,171 +1,176 @@
 // Copyright 2023 Mark Lee (mlee.aus@gmail.com). All rights reserved. MIT license.
-import { stringify, Column } from "https://deno.land/std@0.170.0/encoding/csv.ts";
+import { stringify, Column } from "https://deno.land/std@0.170.0/encoding/csv.ts"
+import { writeAll } from "https://deno.land/std@0.171.0/streams/write_all.ts"
 
 export interface LogOptions {
-  format?: 'csv' | 'json'
-  supressConsoleOutput?: boolean
-  logLabel?: string // <app name>.log.<date-time>.csv|txt
+  format?: 'csv' | 'json' // file extensions respectively are .csv or .txt file
+  disableConsoleLogging?: boolean // don't write to console
+  disableFileLogging?: boolean
+  logLabel?: string // <logLabel>.log.<instantiation-date-time>.csv|txt
+  path?: string // log path defaults to current directory
   maxBytes?: number // default 10mb
 }
 
-export class TinyLogger {
-  #logLabel: string
-  #format: 'csv' | 'json'
-  #supressConsoleOutput: boolean
-  #maxBytes: number
+enum LogLevels {
+  Debug = 'DEBUG',
+  Info = 'INFO',
+  Warn = 'WARN',
+  Error = 'ERROR'
+}
 
-  readonly logInstantiationTime = getFormattedDateTime()
+export class TinyLogger {
+  readonly instantiation = getInstantiation()
+  #format: 'csv' | 'json'
+  #logLabel: string
+  #path: string
+  #disableConsoleLogging: boolean
+  #disableFileLogging: boolean
+  #encoder = new TextEncoder()
+  #maxBytes: number
+  #byteLength = 0
   #currentLogFile = ''
-  #currentLogFileNumber = 0
+  #logFileNumber = 0
 
   #logColors: Map<string, string> = new Map(
     Object.entries(
-      { debug: 'light-blue', info: 'cyan', warn: 'yellow', error: 'red' }
+      { DEBUG: 'light-blue', INFO: 'cyan', WARN: 'yellow', ERROR: 'red' }
     )
   )
 
   constructor(options: LogOptions) {
     this.#format = options.format || 'csv'
-    this.#supressConsoleOutput = options.supressConsoleOutput || false
     this.#logLabel = options.logLabel || 'log'
+    this.#path = options.path || './'
     this.#maxBytes = options.maxBytes || 10485760
+    this.#disableConsoleLogging = options.disableConsoleLogging || false
+    this.#disableFileLogging = options.disableFileLogging || false
+    this.init()
   }
 
-  public async debug(subject: string, message: string) {
-    const level = 'DEBUG'
+  private async init() {
+    if (this.#disableConsoleLogging && this.#disableFileLogging) {
+      throw `TinyLogger Error: file logging and console logging are both disabled.`
+    }
 
-    const data = formatData(this.#format, level, subject, message)
-    await this.writeFile(data, this.#logLabel, this.#format)
+    if (!this.#disableFileLogging) {
+      await this.testFileLogging()
+    }
 
-    if (!this.#supressConsoleOutput) {
-      this.logToConsole(level, subject, message)
+    if (!isAlphanumeric(this.#logLabel)) {
+      throw 'TinyLogger Error: loglabel contains invalid characters. Label can be alphanumeric characters only.'
+    }
+
+    if (this.#logLabel !== 'log') {
+      this.#logLabel = `${this.#logLabel}.log`
+    }
+
+    const initialFile = this.#format === 'csv' ?
+      `${this.#logLabel}.${this.instantiation}.csv` :
+      `${this.#logLabel}.${this.instantiation}.txt`
+
+    this.#currentLogFile = this.#path + initialFile
+  }
+
+  debug(subject: string, message: string) {
+    const data = formatData(this.#format, LogLevels.Debug, subject, message)
+
+    if (!this.#disableConsoleLogging) {
+      this.logToConsole(LogLevels.Debug, subject, message)
+    }
+
+    if (!this.#disableFileLogging) {
+      this.writeFile(data)
     }
   }
 
-  public async info(subject: string, message: string) {
-    const level = 'INFO'
+  info(subject: string, message: string) {
+    const data = formatData(this.#format, LogLevels.Info, subject, message)
 
-    const data = formatData(this.#format, level, subject, message)
-    await this.writeFile(data, this.#logLabel, this.#format)
+    if (!this.#disableConsoleLogging) {
+      this.logToConsole(LogLevels.Info, subject, message)
+    }
 
-    if (!this.#supressConsoleOutput) {
-      this.logToConsole(level, subject, message)
+    if (!this.#disableFileLogging) {
+      this.writeFile(data)
     }
   }
 
-  public async warn(subject: string, message: string) {
-    const level = 'WARN'
+  warn(subject: string, message: string) {
+    const data = formatData(this.#format, LogLevels.Error, subject, message)
 
-    const data = formatData(this.#format, level, subject, message)
-    await this.writeFile(data, this.#logLabel, this.#format)
+    if (!this.#disableConsoleLogging) {
+      this.logToConsole(LogLevels.Error, subject, message)
+    }
 
-    if (!this.#supressConsoleOutput) {
-      this.logToConsole(level, subject, message)
+    if (!this.#disableFileLogging) {
+      this.writeFile(data)
     }
   }
 
-  public async error(subject: string, message: string) {
-    const level = 'ERROR'
+  error(subject: string, message: string) {
+    const data = formatData(this.#format, LogLevels.Error, subject, message)
 
-    const data = formatData(this.#format, level, subject, message)
-    await this.writeFile(data, this.#logLabel, this.#format)
+    if (!this.#disableConsoleLogging) {
+      this.logToConsole(LogLevels.Error, subject, message)
+    }
 
-    if (!this.#supressConsoleOutput) {
-      this.logToConsole(level, subject, message)
+    if (!this.#disableFileLogging) {
+      this.writeFile(data)
+    }
+  }
+
+  private async testFileLogging() {
+    try {
+      await Deno.lstat(this.#path);
+    } catch (err) {
+      if (err instanceof Deno.errors.NotFound) {
+        throw `TinyLogger Error: Invalid Path '${this.#path}'`
+      }
+      throw err;
     }
   }
 
   private logToConsole(
-    level: 'DEBUG' | 'INFO' | 'WARN' | 'ERROR',
+    level: LogLevels,
     subject: string,
     message: string
   ) {
-    const dt = getDateTime()
-    const outputColor = this.#logColors.get(level.toLowerCase())
+    const dt = (new Date()).toISOString()
+    const outputColor = this.#logColors.get(level.toString())
     console.log(`%c[ ${level}, ${dt}, ${subject}, ${message} ]`, `color: ${outputColor};`)
   }
 
-  private async writeFile(data: string, logLabel: string, format: 'csv' | 'json') {
-    if (!isAlphanumeric(logLabel)) {
-      throw 'Error: TinyLogger loglabel contains invalid characters. Label can be alphanumeric characters only.'
-    }
+  private async writeFile(data: string) {
+    const newMessageBytes = this.#encoder.encode(data).byteLength
+    this.#byteLength += newMessageBytes
 
-    let fileToWrite = ''
+    const file = Deno.openSync(
+      this.#currentLogFile,
+      { read: true, write: true, create: true, append: true }
+    )
 
-    let label = logLabel
-    if (logLabel !== 'log') {
-      label = `${logLabel}.log`
-    }
+    if (this.#byteLength > this.#maxBytes) {
+      this.#byteLength = 0
+      this.#logFileNumber++
 
-    if (!this.#currentLogFile) {
-      fileToWrite = format === 'csv' ?
-        `${label}.${this.logInstantiationTime}.csv` :
-        `${label}.${this.logInstantiationTime}.txt`
+      const nextFileToWrite = this.#format === 'csv' ?
+        `${this.#logLabel}.${this.instantiation}_${this.#logFileNumber}.csv` :
+        `${this.#logLabel}.${this.instantiation}_${this.#logFileNumber}.txt`
+
+      this.#currentLogFile = this.#path + nextFileToWrite
+
+      const nextFile = Deno.openSync(
+        this.#currentLogFile,
+        { read: true, write: true, create: true, append: true }
+      )
+
+      await writeAll(nextFile, this.#encoder.encode(data))
+      nextFile.close()
     } else {
-      fileToWrite = this.#currentLogFile
-    }
-
-    try {
-      const file = await Deno.stat(fileToWrite)
-
-      if (file.isFile && file.size > this.#maxBytes) {
-        this.#currentLogFileNumber++
-
-        const nextFileToWrite = format === 'csv' ?
-          `${label}.${this.logInstantiationTime}_${this.#currentLogFileNumber}.csv` :
-          `${label}.${this.logInstantiationTime}_${this.#currentLogFileNumber}.txt`
-
-        this.#currentLogFile = nextFileToWrite
-        fileToWrite = nextFileToWrite
-
-        console.log("current:", this.#currentLogFile)
-        console.log("new:", fileToWrite)
-        console.log("File size exceeded:", file.size);
-
-        try {
-          await Deno.writeTextFile(fileToWrite, data, { append: true })
-        } catch (e) {
-          console.error(e.message)
-        }
-      } else {
-        this.#currentLogFile = fileToWrite
-        try {
-          await Deno.writeTextFile(fileToWrite, data, { append: true })
-        } catch (e) {
-          console.error(e.message)
-        }
-      }
-    } catch (e) {
-      if (e instanceof Deno.errors.NotFound) {
-        try {
-          this.#currentLogFile = fileToWrite
-          await Deno.writeTextFile(fileToWrite, data, { append: true })
-        } catch (e) {
-          console.error(e.message)
-        }
-      } else {
-        console.error(e)
-      }
+      await writeAll(file, this.#encoder.encode(data))
+      file.close()
     }
   }
-}
-
-function getFormattedDateTime() {
-  const dateTime = new Date()
-  const dtArray = dateTime.toJSON().split('T')
-
-  const time = dtArray[1]
-  const timeSansMiliseconds = time.split('.')[0]
-  const timeForFileName = timeSansMiliseconds.replaceAll(':', '-')
-
-  const formattedDateTime = `${dtArray[0]}T${timeForFileName}`
-
-  return formattedDateTime
-}
-
-function getDateTime() {
-  const d = new Date()
-  return d.toISOString().split('.')[0] + 'Z'
 }
 
 function getEol() {
@@ -180,12 +185,12 @@ function isAlphanumeric(s: string) {
 
 function formatData(
   format: 'csv' | 'json',
-  level: string,
+  level: LogLevels,
   subject: string,
   message: string
 ) {
   let data: string
-  const date = getDateTime()
+  const date = (new Date()).toISOString()
 
   switch (format) {
     case 'csv': {
@@ -219,4 +224,17 @@ function formatData(
   }
 
   return data
+}
+
+function getInstantiation() {
+  const dateTime = new Date()
+  const dtArray = dateTime.toJSON().split('T')
+
+  const time = dtArray[1]
+  const timeSansMiliseconds = time.split('.')[0]
+  const timeForFileName = timeSansMiliseconds.replaceAll(':', '-')
+
+  const formattedDateTime = `${dtArray[0]}T${timeForFileName}`
+
+  return formattedDateTime
 }
